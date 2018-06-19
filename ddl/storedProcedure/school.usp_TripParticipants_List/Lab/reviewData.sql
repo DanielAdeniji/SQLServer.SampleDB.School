fd1d4b27d0c90809a083ed17e18d69218f6f02b2
Daniel Adeniji <dadeniji@UCOP.edu>
2018-06-19 08:40:13 -0700

    Added reviewData.sql

---
+++ ddl/storedProcedure/school.usp_TripParticipants_List/Lab/reviewData.sql
@@ -0,0 +1,12 @@
+use [DBLab]
+go
+
+select [src] = '[school].[faculty]', *
+from   [school].[faculty]
+
+select [src] = '[school].[student]', *
+from   [school].[student]
+
+select [src] = '[school].[trip]', *
+from   [school].[trip]
+
